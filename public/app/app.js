3d187227ece54b1b08374c22d1e9bac2a5db867e
Chuy Lara <chuy@jlara.local>
2014-06-01 20:18:49 -0500

    have walking skeleton using angular js

---
+++ public/app/app.js
@@ -0,0 +1,12 @@
+var app = angular.module('app', ['ngResource', 'ngRoute']);
+
+app.config(function($routeProvider, $locationProvider){
+    $locationProvider.html5Mode(true);
+    $routeProvider.
+        when('/', { templateUrl: '/partials/main', controller: 'mainController'} );
+});
+
+app.controller('mainController', function($scope) {
+    $scope.myVar = "Hello Angular";
+});
+
